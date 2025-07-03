67754c196a9f56334d99361889cfb462c8a0fd82
Gloria Enyi <nkemaconam@gmail.com>
2025-07-03 18:47:00 +0100

    updated files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BrainwaveDAO/', // e.g., '/My-Portfolio-Site/'
   plugins: [react()],
+  base: '/BrainwaveDAO/', // e.g., '/My-Portfolio-Site/'
 })

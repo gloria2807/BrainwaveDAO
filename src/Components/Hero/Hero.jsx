5285020ff83086a2be5a07a6c47a1b68e22a2ee2
Gloria Enyi <nkemaconam@gmail.com>
2025-07-04 14:32:59 +0100

    updated file

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import { SiHiveBlockchain } from 'react-icons/si';
 const Hero = () => {
   return (
     <div className='w-full flex md:flex-row flex-col items-center justify-between md:px-[120px] px-5 md:h-[calc(100vh-80px)] relative md:pb-0 pb-20'>
-        <div className='flex flex-col gap-6 md:mt-0 mt-5'>
+        <div className='flex flex-col gap-6 px-4 md:mt-0 mt-5'>
             <div data-aos="fade-up" duration="200" className='flex items-center justify-center gap-2 border border-grayborder rounded-[24px] w-fit px-4 h-[37px]'>
                 <span className='bg-primary w-[9px] h-[9px] rounded-full' />
                 <span className='text-primary text-sm'>Decentralized</span>
@@ -16,10 +16,10 @@ const Hero = () => {
                 Join the token-powered creator collective where your voice counts, your content matters, and your presence earns you perks.
             </p>
             <div data-aos="fade-up" duration="600" className="flex items-center gap-5">
-                <button className='bg-primary text-black text-center cursor-pointer rounded-[40px] w-[150px] h-[50px]  hover:text-primary hover:bg-black hover:border-b-2 hover:border-amber-200'>
+                <button className='bg-primary text-black text-center cursor-pointer rounded-[40px] w-[120px] h-[50px]  hover:text-primary hover:bg-black hover:border-b-2 hover:border-amber-200'>
                     Join the DAO
                 </button>
-                <button className='text-white border-b-2 border-amber-100 text-center cursor-pointer rounded-[40px] w-[190px] h-[50px] hover:text-primary hover:bg-black hover:border-b-2 hover:border-primary'>
+                <button className='text-white border-b-2 border-amber-100 text-center cursor-pointer rounded-[40px] w-[160px] h-[50px] hover:text-primary hover:bg-black hover:border-b-2 hover:border-primary'>
                     Learn How It Works
                 </button>
             </div>
